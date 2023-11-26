f00eaf04f0e3f5e1a911f3c1bdbf226fc169e3ce
NeshGogo <rafaelaguero10@hotmail.com>
2023-11-26 15:42:50 -0400

    Simple api

---
+++ AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleProductInventoryApi.Models;
+
+namespace SampleProductInventoryApi
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Seed();
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
